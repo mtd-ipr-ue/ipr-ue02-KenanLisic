87daefe1a354524e1fdb975a3c34a0a9836f21ea
Kenan Lisic <s2410238037@fhooe.at>
2024-10-28 15:31:40 +0100

    Initial test commit

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,18 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_1 {
-    public static void main(String[] args) {
-        Out.println("Hello, World!");
+    public static void main(String[] args) 
+         {
+        System.out.println("Bitte geben sie 3 ganze Zahlen ein");
+
+        int first = In.readInt();
+        int second = In.readInt();
+        int third = In.readInt();
+
+            int Maximum = (first + second + third);
+            System.out.println("Hier das Maximum der 3 eingegebenen Werte: "+ Maximum);
+                
+            }
     }
-}
+
